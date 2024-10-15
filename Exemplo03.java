fa96d8d573208c21768da0d871ddfe0ff3e054bc
Fc24prog <166566894+Fc24prog@users.noreply.github.com>
2024-10-14 21:55:13 -0300

    Update Exemplo03.java

--- Exemplo03.java
+++ Exemplo03.java
@@ -1,61 +1,60 @@
-package aula1;
+package Alunos;
 import java.util.Scanner;
-public class Exemplo03 {
+public class MediaAlunos{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       Scanner sc= new Scanner(System.in);
-       
-       int identidade;
-   
-       float n1,n2,n3,me,ma;
-       
-       System.out.println("Entre com o numero de identificacao");
-       identidade=sc.nextInt();
-       
-              
-       System.out.println("Entre com a 1º nota");
-       n1=sc.nextFloat();
-       
-       System.out.println("Entre com a 2º nota");
-       n2=sc.nextFloat();
-       
-       System.out.println("Entre com a 3º nota");
-       n3=sc.nextFloat();
-       
-       System.out.println("Entre com a media dos exercicios");
-       me=sc.nextFloat();
-       
-       ma= (n1+n2*2+n3*3+me)/7;
-      System.out.println("A media e: " +ma);
-       
-       if (ma>=9)
-       {
-           System.out.println("conceito A: Aprovado" +ma);
+		Scanner sc= new Scanner(System.in);
+		
+		int identificacao;
+		float n1,n2,n3,mdexercicio,mda;
+		
+		System.out.println("Entre com o numero de identificação do aluno");
+		identificacao=sc.nextInt();
+		
+		System.out.println("Entre com a 1ª nota");
+		n1=sc.nextFloat();
+		
+		System.out.println("Entre com a 2ª nota");
+		n2=sc.nextFloat();
+		
+		System.out.println("Entre com a 3ª nota");
+		n3=sc.nextFloat();
+		
+		System.out.println("Entre com as medias dos exercicios");
+		mdexercicio=sc.nextFloat();
+        
+		mda=(n1*1 + n2*2 + n3*3 + mdexercicio)/7;
+		System.out.println("A media " + mda);
 
-       }
-       
-       if (ma<9 && ma>=7.5)
-       {
-           System.out.println("conceito B: Aprovado" + ma );
+        
+		if (mda>=9) {
+			
+			System.out.println("Conceito A, aprovado" + mda);
+		}
+		else 
+		
+			if (7.5>=mda && mda<9) 
+			{		
+			   System.out.println("Conceito B,aprovado" + mda); 
+			}
+		     else 
+		    	if (6.0>=mda && mda <7.5) 
+		    	{	
+					System.out.println("Conceito C,aprovado" + mda);
+		    	}
 
-       }
-       
-       
-       if (ma<7.5 && ma>=6)
-       {
-           System.out.println("conceito C: Aprovado" + ma );
+			      else 
+				    	if (4.0>=mda && mda <6.0) 
+				    	{   	
+							System.out.println("Conceito D,reprovado" + mda);
+				    	}
+			            else 
+							System.out.println("Conceito E,reprovado" + mda);
+		sc.close();
+		}}		    	
 
-       }
-       
-       if (ma<6 && ma>=4)
-       {
-           System.out.println("conceito D: Reprovado" + ma );
-       }           
-        if(ma<4) {
-       	                 
-                   System.out.println("conceito E: Reprovado" + ma );
+		    
 
-	}      } }   
-             
+		     
 
