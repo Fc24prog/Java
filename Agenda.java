e5a03747ca4f1f371ec59fdcb86b14eb79262a24
Fc24prog <166566894+Fc24prog@users.noreply.github.com>
2024-11-14 10:04:00 -0300

    Update Agenda.java

--- Agenda.java
+++ Agenda.java
@@ -1,10 +1,22 @@
 package main;
-
+import Model.Compromisso;
+import java.util.Scanner;
 public class Agenda {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+// TODO Auto-generated method stub
 
-	}
+Scanner sc= new Scanner (System.in);
+
+       Compromisso cp1 = new Compromisso (12,11,2024,8,50,0,"LP2");
+       System.out.println(cp1);
+       
+      /* Data dt1 = new Data (12,11,2024);
+       Hora h1= new Hora (10,40,00);
+       Compromisso cp2 = new Compromisso(dt1,h1,"IHC");
+       */ /* REVER */
+}
 
 }
+//Data,compromisso e Hora está ligado a agenda
+// agenda é a parte principal
